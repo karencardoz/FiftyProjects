5ef8f0821e5d41e9dc81fd497f62df92b733500e
Karen Cardoz <karen.cardoz@rangle.io>
2023-03-14 14:36:22 -0400

    comments added

--- BlurryImageLoading/script.js
+++ BlurryImageLoading/script.js
@@ -5,12 +5,15 @@ const loadPercent = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
 let load = 0
+// The setInterval() method calls a function at specified intervals (in milliseconds),
+// Until clearInterval() is called, or the window is closed.
 let interval = setInterval(blurring, 30)
 
 //we want this function to run in intervals of evert 30ms
 function blurring() {
   load++
   if (load > 99) {
+    //stop calling the blurring()
     clearInterval(interval)
   }
   //display load%
@@ -20,13 +23,16 @@ function blurring() {
   //if we were starting invisible to opaque then it would be 0 to 1
   loadPercent.style.opacity = scale(load, 0, 100, 1, 0)
   //  now we have to do the same thing for the blur
-  //  In this project I need to use 30 as the max pixels for blur
+  //  In this project, I need to use 30 pixels for max blur
   // template literal = `` and variable or expression syntax
+  // number of times it needs to happen(0-100), we want to map blue from 30 to 0 px
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
+// In our case `opacity` ranges from 0 to 1 and blur from 0px to 30 px, however, load ranges from 0 to 100
+// Hence, we use this  stack overflow () to map a range of numbers to another
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-
+// number of t
 function scale(number, inMin, inMax, outMin, outMax) {
   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
 }
